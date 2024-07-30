cb7cbbb56f2efcad0135c8db7760f8ac240ecf56
Ivan <balmacedaivan5@gmail.com>
2024-07-30 01:46:11 -0300

    mi rutina lista y mi progreso en proceso

---
+++ index.js
@@ -0,0 +1,33 @@
+function success2(){
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "¡Excelente trabajo!👌🏼",
+        text: "Se que puedes dar aun mas.",
+        showConfirmButton: false,
+        timer: 1500
+      });
+}
+  
+function success3(){
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "¡Cada vez mas fuerte!👌🏼",
+        text: "La meta esta cada vez mas cerca",
+        showConfirmButton: false,
+        timer: 1500
+      });
+}
+  
+function success(){
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "¡Ejercicio realizado!💪🏻",
+        text: "Continua progresando asi.",
+        showConfirmButton: false,
+        timer: 1500
+      });
+}
+
